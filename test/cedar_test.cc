3a4c4ea6897c2fcb806ddc20c53e61501f3a0447
Sandeep Joshi <sandeep.joshi@dcengines.net>
2017-07-18 15:07:04 +0530

    amend test to do commonPrefixSearch and commonPrefixPredict

--- test/cedar_test.cc
+++ test/cedar_test.cc
@@ -27,11 +27,28 @@ TEST(cedar, exactMatch) {
 		trie.update(strings[i], strlen(strings[i]), i);
 	}
 
+	EXPECT_EQ(trie.num_keys(), kMaxStrings);
+
 	for (size_t i = 0; i < kMaxStrings; i++) {
 		trie_int_t::result_triple_type result;
 		result = trie.exactMatchSearch<decltype(result)>(strings[i], strlen(strings[i]));
 		EXPECT_EQ(result.value, i);
 		EXPECT_EQ(result.length, strlen(strings[i]));
+	}
+
+	for (size_t i = 0; i < kMaxStrings; i++) {
+		trie_int_t::result_pair_type prefixResult[kMaxStrings];
+		size_t num_res = trie.commonPrefixSearch(strings[i], prefixResult, kMaxStrings);
+		EXPECT_EQ(num_res, i + 1);
+		for (size_t j = 0; j < num_res; j++) {
+			EXPECT_EQ(prefixResult[j].value, j);
+		}
+	}
+
+	for (size_t i = 0; i < kMaxStrings; i++) {
+		trie_int_t::result_triple_type prefixResult[kMaxStrings];
+		size_t num_res = trie.commonPrefixPredict(strings[i], prefixResult, kMaxStrings);
+		EXPECT_EQ(num_res, kMaxStrings - i);
 	}
 
 	{
@@ -43,6 +60,30 @@ TEST(cedar, exactMatch) {
 		trie_int_t::result_triple_type result;
 		result = trie.exactMatchSearch<decltype(result)>("abcdef");
 		EXPECT_EQ(result.value, trie_int_t::CEDAR_NO_VALUE);
+	}
+	{
+		// no inserted strings are prefix of "ab"
+		trie_int_t::result_pair_type prefixResult[kMaxStrings];
+		size_t num_res = trie.commonPrefixSearch("ab", prefixResult, kMaxStrings);
+		EXPECT_EQ(num_res, 0);
+	}
+	{
+		// all inserted strings are prefix of "abcdef"
+		trie_int_t::result_pair_type prefixResult[kMaxStrings];
+		size_t num_res = trie.commonPrefixSearch("abcdef", prefixResult, kMaxStrings);
+		EXPECT_EQ(num_res, kMaxStrings);
+	}
+	{
+		// all inserted strings are completions of "ab"
+		trie_int_t::result_triple_type prefixResult[kMaxStrings];
+		size_t num_res = trie.commonPrefixPredict("ab", prefixResult, kMaxStrings);
+		EXPECT_EQ(num_res, kMaxStrings);
+	}
+	{
+		// no inserted strings are completions of "abcdef"
+		trie_int_t::result_triple_type prefixResult[kMaxStrings];
+		size_t num_res = trie.commonPrefixPredict("abcdef", prefixResult, kMaxStrings);
+		EXPECT_EQ(num_res, 0);
 	}
 }
 
